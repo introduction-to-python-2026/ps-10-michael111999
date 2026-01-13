99b39e9829395797de2a5830a125e739da4a3882
michael111999 <micsasson7@gmail.com>
2026-01-13 18:50:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from skimage.morphology import ball
 
 
 load_image('smurf.jpg')
+from skimage.filters import median
+from skimage.morphology import ball
 
+clean_image = median(image, ball(3))
+edge_detection('smurf.jpg')
                 
                                  
                                  
